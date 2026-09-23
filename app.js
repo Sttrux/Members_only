8f5789f31678602c9e71e0c39ef70687f37e4970
Strux <ghost.strux@gmail.com>
2026-09-22 22:02:11 -0500

    port fix

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(passport.session());
 app.use("/", index_route);
 
 //Listening PORT
-const PORT = process.env.LISTEN_PORT || 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App running on PORT ${PORT}`);
 });
